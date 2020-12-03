e2e2015cc06d574c217c895b29b3ba38413a5dfc
Dinesh <dineshmj82@gmail.com>
2020-12-03 22:11:44 +0530

    add .files

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,7 @@
 import Axios from 'axios';
-const url = "http://localhost:2000/"
+
+// const url = "http://localhost:2000/"
+const url = "https://spe-node.herokuapp.com/"
 
 export const http =  Axios.create({
     baseURL:url
