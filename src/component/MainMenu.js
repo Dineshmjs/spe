ec76ea3ee187ec44e6a23f6f1f119d4a79c1e791
Dinesh <dineshmj82@gmail.com>
2020-12-04 21:43:30 +0530

    add material-ui and change title in mainmenu

--- src/component/MainMenu.js
+++ src/component/MainMenu.js
@@ -12,19 +12,19 @@ function MainMenu() {
             <div className="w3-row-padding">
                 <div className="w3-third">
                     <div className="w3-card">
-                        <h4 className="w3-center w3-text-green" >Billing</h4>
+                        <h4 className="w3-center w3-text-green" >Sales</h4>
                         <Link to="sales"><img src={billingImg} alt="billing" id="img" /></Link>
                     </div>
                 </div>
                 <div className="w3-third">
                     <div className="w3-card">
-                        <h4 className="w3-center w3-text-green" >New Entry</h4>
+                        <h4 className="w3-center w3-text-green" >Purches</h4>
                         <Link to="purches"><img src={updateImg} alt="update" id="img" /></Link>
                     </div>
                 </div>
                 <div className="w3-third">
                     <div className="w3-card">
-                        <h4 className="w3-center w3-text-green" >Transactions</h4>
+                        <h4 className="w3-center w3-text-green" >Tax</h4>
                         <Link to="bills"><img src={showImg} alt="show" id="img" /></Link>
                     </div>
                 </div>
