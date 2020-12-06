0fab8b6c41d769221c1c50f2f5126efb19c94f37
Dinesh <dineshmj82@gmail.com>
2020-12-06 11:05:34 +0530

    Router Add Purches Path

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Main from '../component/'
-import  Purches from '../component/purches'
+
 import FinalBill from '../component/sales/newbill/bill/FinalBill'
+
 import SalesInvoice from '../component/sales/invoice'
-import NewBill from '../component/sales/newbill'
-import Buyers from '../component/sales/buyers'
+import SalesNewBill from '../component/sales/newbill'
+import SalesBuyers from '../component/sales/buyers'
+
+import PurchesInvoice from '../component/purches/invoice'
+import PurchesNewBill from '../component/purches/newbill'
+import PurchesBuyers from '../component/purches/buyers'
+import PurchesProducts from '../component/purches/products'
 
 function index() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component = {Main} />               
-                <Route exact path="/purches" component = {Purches} />
+                
                 <Route exact path="/final" component={FinalBill} />
-                <Route exact path="/sales/newbill" component={NewBill} />
+
+                <Route exact path="/sales/newbill" component={SalesNewBill} />
                 <Route exact path="/sales" component={SalesInvoice} />
-                <Route exact path="/sales/buyers" component={Buyers} />
+                <Route exact path="/sales/buyers" component={SalesBuyers} />
+
+                <Route exact path="/purches" component = {PurchesInvoice} />
+                <Route exact path="/purches/newbill" component={PurchesNewBill} />
+                <Route exact path="/purches/buyers" component={PurchesBuyers} />
+                <Route exact path="/purches/products" component={PurchesProducts} />
+
+
             </Switch>
         </BrowserRouter>
     )
